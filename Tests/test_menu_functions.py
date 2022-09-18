f3e6e9c8be1cddce8d5094e72aa2ee33183da957
James Butcher <jmsbutcher1576@gmail.com>
2022-09-18 17:34:00 -0400

    Added create new text file if doesnt exist. Cannot get it to pass unit tests. File paths and names too complex. Need to simplify how to do this.

--- Tests/test_menu_functions.py
+++ Tests/test_menu_functions.py
@@ -1,12 +1,19 @@
 
+import glob
 import io
+import shutil
 import sys
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
 sys.path.append("../../KnowledgeManager")
 
 from menu_functions import *
+
+
+TEST_DIRECTORY_NAME = "test_dir"
+TEST_DIRECTORY_PATH = TEST_FOLDER_PATH / Path(TEST_DIRECTORY_NAME)
 
 #------------------------------------------------------------------------------
 
@@ -26,6 +33,14 @@ def capture_output(func):
     sys.stdout = sys.__stdout__
     return capturedOutput.getvalue()
 
+def set_up_test_dir():
+    if os.path.exists(TEST_DIRECTORY_PATH):
+        shutil.rmtree(TEST_DIRECTORY_PATH)
+    os.mkdir(TEST_DIRECTORY_PATH)
+
+
+def tear_down_test_dir():
+    shutil.rmtree(TEST_DIRECTORY_PATH)
 
 
 class TestMenu(unittest.TestCase):
@@ -51,9 +66,102 @@ class TestMenu(unittest.TestCase):
     def test_create_new_document_folder_if_doesnt_exist(self):
         """ Test that create_new_document_fold... creates a new doc folder """
 
-        os.mkdir("test_new_doc_folder")
-        os.chdir("test_new_doc_folder")
+
+        set_up_test_dir()
+        os.chdir(TEST_DIRECTORY_PATH)
+
+        # Test that the folder doesn't exist first ( ../Tests/test_dir/km_documents )
         self.assertFalse(os.path.exists(DOCUMENT_FOLDER_NAME))
+
+        # Now call the function to create the folder
+        create_new_document_folder_if_doesnt_exist()
+
+        # Test that the document folder has been created
+        self.assertTrue(os.path.exists(DOCUMENT_FOLDER_NAME))
+
+        # Make sure a system error doesn't happen here as a result of
+        # trying to create a folder that already exists
+        output = capture_output(create_new_document_folder_if_doesnt_exist)
+        self.assertEqual(output, "")
+
+        os.chdir(HOME)
+        tear_down_test_dir()
+
+
+    def test_check_if_filename_exists_returns_false_when_doesnt_exist(self):
+        """ 
+        Test check_if_filename_exists function returns true if a filename
+        exists in the target directory and false otherwise.
+        """
+
+        set_up_test_dir()
+
+        non_existent_filename = "nothing"
+        self.assertFalse(check_if_filename_exists(TEST_DIRECTORY_PATH / non_existent_filename))
+
+        tear_down_test_dir()
+
+
+    def test_check_if_filename_exists_returns_true_when_does_exist(self):
+        """ 
+        Test check_if_filename_exists function returns true if a filename
+        exists in the target directory and false otherwise.
+        """
+
+        set_up_test_dir()
+
+        filename = "file.txt"
+        with open(TEST_DIRECTORY_PATH / filename, "w") as f:
+            self.assertTrue(check_if_filename_exists(TEST_DIRECTORY_PATH / filename))
+
+        tear_down_test_dir()
+
+
+    def test_create_new_text_file_if_doesnt_exist_with_provided_name(self):
+        """ Test that a new text document gets created with the correct name"""
+
+        set_up_test_dir()
+        os.chdir(TEST_DIRECTORY_PATH) # HOME / Tests / test_dir
+
+        # Create a mock documents folder first, as used in handle_create_new_document
+        create_new_document_folder_if_doesnt_exist() # HOME / Tests / test_dir / km_documents
+
+        # Create the file and test that it exists
+        TEST_FILE_NAME = "test_document"
+        TEST_FILE_PATH = TEST_DIRECTORY_PATH / DOCUMENT_FOLDER_NAME
+        create_new_text_file_if_doesnt_exist(TEST_FILE_PATH, TEST_FILE_NAME) # HOME / Tests / test_dir / km_documents / test_document
+        self.assertTrue(os.path.exists(TEST_FILE_PATH, TEST_FILE_NAME))
+
+        os.chdir(HOME)
+        tear_down_test_dir()
+
+
+    def test_create_new_text_file_if_doesnt_exist_gives_error_message_if_exists(self):
+        """
+        Test that creating a file that has the same name as an already
+        existing file results in an error message and no new file being
+        created.
+        """
+
+        set_up_test_dir()
+        os.chdir(TEST_DIRECTORY_PATH)
+
+        # Create a mock documents folder first, as used in handle_create_new_document
+        create_new_document_folder_if_doesnt_exist()
+
+        # Create the file and test that it exists
+        TEST_FILE_NAME = "test_document"
+        TEST_FILE_PATH = TEST_DIRECTORY_PATH / DOCUMENT_FOLDER_NAME
+        create_new_text_file_if_doesnt_exist(TEST_FILE_PATH, TEST_FILE_NAME)
+        self.assertTrue(os.path.exists(TEST_FILE_PATH / TEST_FILE_NAME))
+
+        # Try creating a file with the same name, and make sure an error message
+        # gets displayed.
+        output = capture_output(create_new_text_file_if_doesnt_exist(TEST_FILE_PATH, TEST_FILE_NAME))
+        self.assertIn(output.lower(), "error")
+
+        os.chdir(HOME)
+        tear_down_test_dir()
         
 
 #------------------------------------------------------------------------------
--- menu_functions.py
+++ menu_functions.py
@@ -1,8 +1,12 @@
 
 import os
+from pathlib import Path
 
-
+HOME = Path(os.getcwd())
 DOCUMENT_FOLDER_NAME = "km_Documents"
+DOCUMENT_FOLDER_PATH = HOME / Path(DOCUMENT_FOLDER_NAME)
+TEST_FOLDER_NAME = "Tests"
+TEST_FOLDER_PATH = HOME / Path(TEST_FOLDER_NAME)
 
 
 def display_greeting():
@@ -48,9 +52,26 @@ def handle_choice(choice):
 
 def handle_create_new_document():
     create_new_document_folder_if_doesnt_exist()
+
+    print
+
     print("New document created.")
 
 
 def create_new_document_folder_if_doesnt_exist():
     if not os.path.exists(DOCUMENT_FOLDER_NAME):
         os.mkdir(DOCUMENT_FOLDER_NAME)
+
+
+def check_if_filename_exists(file_path):
+    return os.path.exists(file_path)
+
+
+def create_new_text_file_if_doesnt_exist(file_path, filename):
+    if not check_if_filename_exists(file_path / filename):
+        new_file = filename + ".txt"
+        with open(file_path / new_file, "w") as f:
+            pass
+        print("New text file created: ",  new_file)
+    else:
+        print("Error - a file with that name already exists. Try again.")
