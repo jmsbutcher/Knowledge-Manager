970b7aef6f2796ed734e493de49f98b820877ab9
James Butcher <jmsbutcher1576@gmail.com>
2022-11-26 10:14:03 -0500

    Modified root directory string in globals to use cwd instead of hard-coded path, so now it works on my mac in addition to my PC

--- KnowledgeManager/globals.py
+++ KnowledgeManager/globals.py
@@ -1,7 +1,9 @@
 
 from pathlib import Path
+import os
 
-ROOT = Path("C:/Users/James/Documents/Programming/KnowledgeManager")
+#ROOT = Path("C:/Users/James/Documents/Programming/KnowledgeManager")
+ROOT = Path(os.getcwd()).parent
 SOURCE_PATH = ROOT / "KnowledgeManager"
 TEST_PATH = ROOT / "Tests"
 DEFAULT_DOCUMENT_REPO_PATH = SOURCE_PATH / "km_Documents"
